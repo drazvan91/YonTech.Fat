6cf2905ad5476fd2996fe361ea9f119de4ee051d
Razvan Dragomir <razvan.dragomir@tss-yonder.com>
2020-02-25 00:37:35 +0200

    Add Travis yml file

--- Yontech.Fat.Templates/templates/CreateFatProject/Config.cs
+++ Yontech.Fat.Templates/templates/CreateFatProject/Config.cs
@@ -7,6 +7,7 @@ namespace CreateFatProject
         public Config()
         {
             Browser = BrowserType.Chrome;
+            RunInBackground = true;
         }
     }
 }
--- Yontech.Fat.Templates/templates/CreateFatProjectWithSamples/Config.cs
+++ Yontech.Fat.Templates/templates/CreateFatProjectWithSamples/Config.cs
@@ -7,6 +7,7 @@ namespace CreateFatProjectWithSamples
         public Config()
         {
             Browser = BrowserType.Chrome;
+            RunInBackground = true;
         }
     }
 }
