417efd99f78a96bb9d910263aebb1baa53b410ca
Razvan Dragomir <drazvan91@gmail.com>
2020-04-19 22:36:44 +0300

    Add unit test for Default Config file

---
+++ tests/Yontech.Fat.Tests/RunnerTests/RunWithDefaultConfigTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using Yontech.Fat.Discoverer;
+using Yontech.Fat.Logging;
+using Yontech.Fat.Runner;
+using Yontech.Fat.Utils;
+
+namespace Yontech.Fat.Tests.RunnerTests
+{
+    public class RunWithDefaultConfigTests
+    {
+        private readonly MockedLoggerFactory mockedLoggerFactory;
+        private readonly FatRunner runner;
+
+        public RunWithDefaultConfigTests()
+        {
+            mockedLoggerFactory = new MockedLoggerFactory();
+            var streamProvider = new StreamProvider(mockedLoggerFactory);
+            MockedAssemblyDiscoverer assemblyDiscoverer = new MockedAssemblyDiscoverer(typeof(Beta.Class1).Assembly);
+
+            runner = new FatRunner(assemblyDiscoverer, mockedLoggerFactory, streamProvider, (options) =>
+            {
+            });
+        }
+
+        [Fact]
+        public void When_run_with_default_settings_Then_happy_flow()
+        {
+            var result = runner.Run();
+
+            Assert.Equal(0, result.Failed);
+            Assert.Equal(1, result.Passed);
+        }
+    }
+}
