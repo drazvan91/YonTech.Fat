fdb5237609f82fc9b38b79fc93212c26fe1a56b9
Razvan Dragomir <drazvan91@gmail.com>
2020-05-05 23:09:52 +0300

    Add documentation about FatLabels

--- docs-src/doczrc.js
+++ docs-src/doczrc.js
@@ -1,14 +1,15 @@
 // doczrc.js
 export default {
-    base: '/YonTech.Fat',
-    menu: [
-        'Introduction',
-        'Getting Started',
-        'Run configuration',
-        'Concepts',
-        'API',
-        "Samples"
-    ], 
-    title: "Fat Framework",
-    dest: "../docs"
-  }
+  base: '/YonTech.Fat',
+  menu: [
+    'Introduction',
+    'Getting Started',
+    'Run configuration',
+    'Concepts',
+    'Fat Label',
+    'API',
+    'Samples'
+  ],
+  title: 'Fat Framework',
+  dest: '../docs'
+}
