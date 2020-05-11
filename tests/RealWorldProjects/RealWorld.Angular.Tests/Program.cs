3927ed4bc72b17c524f0388eff710bea651033a6
Razvan Dragomir <drazvan91@gmail.com>
2020-05-11 22:24:17 +0300

    Add doc page for FatLogging

--- tests/RealWorldProjects/RealWorld.Angular.Tests/Program.cs
+++ tests/RealWorldProjects/RealWorld.Angular.Tests/Program.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using Yontech.Fat.Runner;
-
-using System.Collections.Generic;
-using System.Reflection;
-using Yontech.Fat;
-using Yontech.Fat.Interceptors;
-using RealWorld.Angular.Tests.Interceptors;
-using System.Net.Http;
-using System.IO;
-using System.IO.Compression;
-using RealWorld.Angular.Tests.TestCases;
+﻿
 using Yontech.Fat.ConsoleRunner;
 
 namespace RealWorld.Angular.Tests
