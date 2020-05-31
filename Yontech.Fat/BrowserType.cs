164da17f1ef4c5e29bcc5434217bcacb0f9811e9
Razvan Dragomir <drazvan91@gmail.com>
2020-05-31 21:07:16 +0300

    Add Chrome drivers for v81, v83 and v84

--- Yontech.Fat/BrowserType.cs
+++ Yontech.Fat/BrowserType.cs
@@ -20,6 +20,12 @@ namespace Yontech.Fat
         v79 = 79,
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "It describes the version")]
         v80 = 80,
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "It describes the version")]
+        v81 = 81,
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "It describes the version")]
+        v83 = 83,
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "It describes the version")]
+        v84 = 84,
     }
 
     public enum FirefoxVersion : int
--- Yontech.Fat/Selenium/DriverFactories/ChromeDriverDownloader.cs
+++ Yontech.Fat/Selenium/DriverFactories/ChromeDriverDownloader.cs
@@ -9,6 +9,9 @@ namespace Yontech.Fat.Selenium.DriverFactories
         private const string CHROME_74_FOLDER = "http://chromedriver.storage.googleapis.com/74.0.3729.6/";
         private const string CHROME_79_FOLDER = "http://chromedriver.storage.googleapis.com/79.0.3945.16/";
         private const string CHROME_80_FOLDER = "http://chromedriver.storage.googleapis.com/80.0.3987.16/";
+        private const string CHROME_81_FOLDER = "https://chromedriver.storage.googleapis.com/81.0.4044.138/";
+        private const string CHROME_83_FOLDER = "https://chromedriver.storage.googleapis.com/83.0.4103.39/";
+        private const string CHROME_84_FOLDER = "https://chromedriver.storage.googleapis.com/84.0.4147.30/";
         private const string MACOS64 = "chromedriver_mac64.zip";
         private const string WIN32 = "chromedriver_win32.zip";
         private const string LINUX64 = "chromedriver_linux64.zip";
@@ -37,7 +40,10 @@ namespace Yontech.Fat.Selenium.DriverFactories
                 case ChromeVersion.v74: return CHROME_74_FOLDER;
                 case ChromeVersion.v79: return CHROME_79_FOLDER;
                 case ChromeVersion.v80: return CHROME_80_FOLDER;
-                default: return CHROME_80_FOLDER;
+                case ChromeVersion.v81: return CHROME_81_FOLDER;
+                case ChromeVersion.v83: return CHROME_83_FOLDER;
+                case ChromeVersion.v84: return CHROME_84_FOLDER;
+                default: return CHROME_84_FOLDER;
             }
         }
 
